af250ca4fbe8fffe2ca07d33564f51b1805f0894
Enock Jeremi <enockjeremi@gmail.com>
2023-10-14 18:20:12 -0300

    fix for deploy 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { enviroments } from './enviroments';
 import config from './config';
-// import * as Joi from 'joi';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -12,13 +12,13 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
-      // validationSchema: Joi.object({
-      //   DB_HOST: Joi.string().required(),
-      //   DB_PORT: Joi.string().required(),
-      //   DB_USER: Joi.string().required(),
-      //   DB_PASSWORD: Joi.string().required(),
-      //   DB_NAME: Joi.string().required(),
-      // }),
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     ReportsModule,
     DatabaseModule,
